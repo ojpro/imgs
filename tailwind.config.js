a1aaed5bffa58ea81b8d183f5ed564310a8b769d
ojpro <oussamaeljabbari.mail@gmail.com>
2021-09-17 22:36:34 +0100

    Added tailwindcss to this project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [		'./resources/**/*.blade.php',
+		'./resources/**/*.{js,ts,vue}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
