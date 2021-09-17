6193312d2039ac3f76f1efbf1da8006ebe1ef855
ojpro <oussamaeljabbari.mail@gmail.com>
2021-09-17 22:36:03 +0100

    Config Vue

---
+++ resources/js/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from '@/views/App.vue'
+import '@/css/app.css'
+import router from './router'
+
+createApp(App).use(router).mount('#app')
---
+++ resources/views/App.vue
@@ -0,0 +1,6 @@
+<template>
+ <router-link to="/" class="mx-2 bg-red-400 text-white p-2">Home</router-link>
+ <router-link to="/about">About</router-link>
+ <router-view></router-view>
+</template>
+
