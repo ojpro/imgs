a12262298daa545d10ea187af7577de5dd46e6b0
ojpro <oussamaeljabbari.mail@gmail.com>
2021-09-18 16:00:05 +0100

    Added Images migration table

---
+++ database/migrations/2021_09_17_223616_create_images_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->char('label', 200);
+            $table->char('url', 200)->unique();
+            $table->char('category_name', 150);
+            $table->timestamps();
+
+            $table->foreign('category_name')
+                ->references('name')
+                ->on('categories')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
