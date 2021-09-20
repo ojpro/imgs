75023c3447414800c734fe37d1ed7cb503b5e78e
ojpro <oussamaeljabbari.mail@gmail.com>
2021-09-20 14:02:24 +0100

    Using jit mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  purge: [		'./resources/**/*.blade.php',
-		'./resources/**/*.{js,ts,vue}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    mode: "jit",
+    purge: ["./resources/views/*.blade.php", "./resources/**/*.{js,ts,vue}"],
+    // darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
