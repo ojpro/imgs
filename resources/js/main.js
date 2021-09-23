c3f43fe05d2baa5897720e8aa48dc53d44809441
ojpro <oussamaeljabbari.mail@gmail.com>
2021-09-23 17:17:53 +0100

    Import sweatlaert in main js file

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import App from '@/views/App.vue'
 import '@/css/app.css'
 import router from './router'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 createApp(App)
-.use(router).
-mount('#app')
+.use(VueSweetalert2)
+.use(router)
+.mount('#app')
