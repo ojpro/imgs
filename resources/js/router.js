0a07680a23b965cf24f1d238abd7fe50941848c9
ojpro <oussamaeljabbari.mail@gmail.com>
2021-09-23 17:15:12 +0100

    Lazy load components

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,11 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "@/views/pages/Home.vue";
-
 const routes = [
     {
         path: "/",
         name: "Home",
-        component: Home,
+        component: () => import('@/views/pages/Home.vue'),
     },
     {
         path: "/upload",
