f64cd489d6867ba22dca2905ad08208bdd582c67
ojpro <oussamaeljabbari.mail@gmail.com>
2021-09-25 01:30:45 +0100

    Addedaction dropdown with delete and copyurl functionality

--- resources/views/partials/Gallery.vue
+++ resources/views/partials/Gallery.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="w-11/12 mx-auto my-4">
     <div class="my-4 flex flex-row justify-between">
-      <form action="" @submit.prevent="fetchImages()" class="w-1/3">
+      <form
+        action=""
+        @submit.prevent="fetchImages()"
+        class="w-1/2 md:w-1/3 xl:1/4"
+      >
         <input
           type="search"
           list="images"
@@ -31,6 +35,7 @@
         </router-link>
       </div>
     </div>
+    <!-- Images Gallery -->
     <ul
       class="relative col-count-1 sm:col-count-2 md:col-count-3 xl:col-count-4"
     >
@@ -74,32 +79,83 @@
         </div>
       </li>
     </ul>
+    <!-- Slider -->
     <div
-      class="fixed inset-0 h-auto overflow-auto p-8 bg-gray-800/25"
+      class="fixed inset-0 h-screen overflow-auto p-8 bg-gray-800/25"
       v-show="openSlider"
       @click="openSlider = false"
     >
       <img
         :src="slide.url"
-        class="w-10/12 mx-auto my-4"
+        class="
+          w-11/12
+          md:w-8/12
+          absolute
+          left-1/2
+          top-1/2
+          -translate-x-1/2 -translate-y-1/2
+        "
         alt=""
         loading="lazy"
         @click.stop=""
       />
-      <div class="absolute inset-0 z-[-1]">
-        <span
-          class="float-right m-4 cursor-pointer text-white hover:text-red-400"
-          >X</span
-        >
-        <div class="fixed left-8 top-8 z-20 m-1 leading-8">
-          <span class="text-white text-md font-bold leading-relaxed cursor-pointer bg-red-400" @click.stop="openDropdown = !openDropdown">...</span>
-          <div class="bg-white rounded p-2 font-medium" v-show="openDropdown">
-            <ul>
-              <li class="m-1"><a href="" class="text-gray-600 hover:text-gray-800">Share</a></li>
-              <li class="m-1"><a href="" class="text-gray-600 hover:text-gray-800">Edit</a></li>
-              <li class="m-1"><a href="" class="text-red-600 hover:text-red-800">Delete</a></li>
-            </ul>
+      <!-- Slider dropdown -->
+      <div class="absolute left-0 top-0 right-0 z-30 p-4">
+        <div class="flex flex-wrap flex-row justify-between">
+          <div class="block">
+            <span
+              class="
+                text-white text-2xl
+                font-black
+                leading-relaxed
+                cursor-pointer
+              "
+              @click.stop="openDropdown = !openDropdown"
+              >...</span
+            >
+            <div
+              class="bg-white rounded p-2 font-medium shadow"
+              v-show="openDropdown"
+            >
+              <ul>
+                <li class="m-1">
+                  <a
+                    :href="slide.url"
+                    target="_blank"
+                    class="text-gray-600 hover:text-gray-800"
+                  >
+                    Open in new tab
+                  </a>
+                </li>
+                <li class="m-1">
+                  <a
+                    :href="slide.url"
+                    class="text-gray-600 hover:text-gray-800"
+                    @click.stop.prevent="copyURL(slide.url)"
+                  >
+                    Copy link
+                  </a>
+                </li>
+                <li class="m-1" v-show="slide">
+                  <router-link
+                    :to="{ name: 'Edit', params: { id: slide.id } }"
+                    class="text-gray-600 hover:text-gray-800"
+                  >
+                    Edit Image
+                  </router-link>
+                </li>
+                <li class="m-1">
+                  <a
+                    href="#"
+                    class="text-red-600 hover:text-red-800"
+                    @click.prevent="deleteImage(slide.id)"
+                    >Delete</a
+                  >
+                </li>
+              </ul>
+            </div>
           </div>
+          <span class="cursor-pointer text-white hover:text-red-400">X</span>
         </div>
       </div>
     </div>
@@ -115,7 +171,7 @@ export default {
       search: "",
       openSlider: false,
       slide: "",
-      openDropdown:false
+      openDropdown: false,
     };
   },
   methods: {
@@ -127,6 +183,74 @@ export default {
     showSlider(val) {
       this.openSlider = true;
       this.slide = val;
+      this.openDropdown = false;
+    },
+    deleteImage(id) {
+      this.$swal
+        .fire({
+          icon: "warning",
+          title: "Delete this image?",
+          text: "This action can't be undo.",
+          confirmButtonText: "Delete it",
+          confirmButtonColor: "red",
+          confirmButtonBorder: "red",
+          showCancelButton: true,
+        })
+        .then((result) => {
+          if (result.isConfirmed) {
+            axios.delete("api/images/" + id).then((response) => {
+              if (response.data.success) {
+                // Show success alert
+                const Toast = this.$swal.mixin({
+                  toast: true,
+                  position: "top-end",
+                  showConfirmButton: false,
+                  timer: 2500,
+                  timerProgressBar: true,
+                  didOpen: (toast) => {
+                    toast.addEventListener("mouseenter", this.$swal.stopTimer);
+                    toast.addEventListener(
+                      "mouseleave",
+                      this.$swal.resumeTimer
+                    );
+                  },
+                });
+                Toast.fire({
+                  title: response.data.success,
+                  icon: "success",
+                });
+
+                this.fetchImages();
+              } else {
+                this.$swal.fire({
+                  icon: "error",
+                  title: response.data.error,
+                });
+              }
+            });
+          }
+        });
+    },
+    copyURL(url) {
+      this.$copyText(url).then((_) => {
+        // Show success alert
+        const Toast = this.$swal.mixin({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 2500,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener("mouseenter", this.$swal.stopTimer);
+            toast.addEventListener("mouseleave", this.$swal.resumeTimer);
+          },
+        });
+        Toast.fire({
+          title: "Image's url copied!.",
+          icon: "success",
+        });
+        this.openDropdown = false;
+      });
     },
   },
   created() {
