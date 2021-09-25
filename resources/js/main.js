6b889b10a0a8cc53b6042338530df61393002d0f
ojpro <oussamaeljabbari.mail@gmail.com>
2021-09-25 01:29:12 +0100

    Adde vue-clipboard2

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,11 +1,9 @@
-import { createApp } from 'vue'
-import App from '@/views/App.vue'
-import '@/css/app.css'
-import router from './router'
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
+import { createApp } from "vue";
+import App from "@/views/App.vue";
+import "@/css/app.css";
+import router from "./router";
+import VueSweetalert2 from "vue-sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+import VueClipboard from "vue-clipboard2";
 
-createApp(App)
-.use(VueSweetalert2)
-.use(router)
-.mount('#app')
+createApp(App).use(VueClipboard).use(VueSweetalert2).use(router).mount("#app");
