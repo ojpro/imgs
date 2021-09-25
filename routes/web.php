86a0c5e809023d04a4de71fa8ce3be2b58fde86c
ojpro <oussamaeljabbari.mail@gmail.com>
2021-09-25 01:29:36 +0100

    Fix routing problem

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/img/{shortURLKey}', ShortURLController::class);
+
 Route::get('/{any?}',function(){
     return view('app');
-});
+})->where('any','.*');
 
-Route::get('/img/{shortURLKey}', ShortURLController::class);
